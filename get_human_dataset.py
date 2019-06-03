f4de5d739a7e72fbbb9986b1b259a687b4e33ed8
송석리(Song Sukree) <greatsong21@gmail.com>
2019-06-03 17:07:53 +0900

    Create get_human_dataset.py

---
+++ get_human_dataset.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+def get_human_dataset( ):
+    
+    # 각 데이터 파일들은 공백으로 분리되어 있으므로 read_csv에서 공백 문자를 sep으로 할당.
+    feature_name_df = pd.read_csv('./features.txt',sep='\s+',
+                        header=None,names=['column_index','column_name'])
+    # DataFrame에 피처명을 컬럼으로 부여하기 위해 리스트 객체로 다시 변환
+    feature_name = feature_name_df.iloc[:, 1].values.tolist()
+    
+    # 학습 피처 데이터 셋과 테스트 피처 데이터을 DataFrame으로 로딩. 컬럼명은 feature_name 적용
+    X_train = pd.read_csv('./train/X_train.txt',sep='\s+', names=feature_name)
+    X_test = pd.read_csv('./test/X_test.txt',sep='\s+', names=feature_name)
+    
+    # 학습 레이블과 테스트 레이블 데이터을 DataFrame으로 로딩하고 컬럼명은 action으로 부여
+    y_train = pd.read_csv('./train/y_train.txt',sep='\s+',header=None,names=['action'])
+    y_test = pd.read_csv('./test/y_test.txt',sep='\s+',header=None,names=['action'])
+    
+    # 로드된 학습/테스트용 DataFrame을 모두 반환 
+    return X_train, X_test, y_train, y_test
+
+
+X_train, X_test, y_train, y_test = get_human_dataset()
